db01882772ec494c37219bef4c6907f7925f8871
AdrianaVB <135856571+AdrianaVB@users.noreply.github.com>
2023-09-05 21:55:28 +0200

    Modificación de ejercicios

--- Semestre2AdryBalmaceda/Semestre2-AdryBalmaceda-Python2/Ejercicio1.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio 1: Iterar un rango de 0 a 10 e imprimir numeros divisibles entre 3
-# Ejemplo de ejecucion: 0,3,6,9
-print('Rango 0 a 10 con Numeros divisibles entre 3')
-for i in range(11):
-    if i % 3 == 0:
-        print(i)
--- Semestre2AdryBalmaceda/Semestre2-AdryBalmaceda-Python2/Ejercicio2.py
+++
@@ -1,2 +0,0 @@
-# Ejercicio 2: Crear im rango de numeros de 2 a 6 e imprimir
-# Ejemplo de ejecucion: 2,3,4,5,6
