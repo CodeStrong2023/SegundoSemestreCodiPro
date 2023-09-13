60cb31d7794bcd9d07247643a54fd6ff41ba3df7
Siro01 <136366859+Siro01@users.noreply.github.com>
2023-09-13 18:48:24 -0300

    Ejercicios de la leccion 4.

---
+++ EjerciciosPython-Siro.T/Leccion4Ejercicios/main.py
@@ -0,0 +1,15 @@
+#Ejercicio 1 iterar un rango de 0 a 10 e imprimir numeros divisibles entre 3.
+
+for numeros in range (0,11,3):
+    print(numeros)
+
+print('')
+#Ejercicio 2 iterar un rango de 2 a 6 e imprimirlos.
+for numeros in range (2,7):
+    print(numeros)
+
+print('')
+#Ejercicio 3 crear un rango de a 3 a 10 pero con incremento de 2 en 2.
+
+for numeros in range(3,11,2):
+    print(numeros)
