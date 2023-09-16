6792e9dc0f6014b79fac6f54898fa73730502219
AVBalmaceda <aadriveross@gmail.com>
2023-09-16 13:19:30 +0200

    Práctica de Tipos de datos en Javascript

--- Adry-Balmaceda/JavaScript-Adry-Balmaceda/Leccion2/02-01-TiposDatos.js
+++ Adry-Balmaceda/JavaScript-Adry-Balmaceda/Leccion2/02-01-TiposDatos.js
@@ -5,14 +5,14 @@ es muy similar a la de Java
 realmente diriamos que es identica
 */
 // Datos de tipo String
-var nombre = 'Adry';
+var nombre = 'Adriana';
 console.log(nombre);
 // Tipo numerico
 var numero = 3000;
 console.log(numero);
 
 var objeto = {
-    nombre : 'Adriana',
+    nombre : 'Adry',
     apellido : 'Balmaceda',
     telefono : '261000300',
 }
