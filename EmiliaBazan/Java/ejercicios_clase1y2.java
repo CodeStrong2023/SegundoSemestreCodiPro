7686711a988aa27b83de64c44b51f903aeca26ba
Emilia Bazan <e.bazanazargado@gmail.com>
2023-09-18 21:54:33 -0300

    add notes

---
+++ EmiliaBazan/Java/ejercicios_clase1y2.java
@@ -0,0 +1,27 @@
+/*Ejercicio 3: Leer nros hasta que se introduzca un cero. Para cada uno indicar si es par o impar.
+Primero con la clase scanner, luego con la clase JOptionPane*/
+
+/*Ejercicio 4: TEclear números hasta que se introduzca uno negativo y mostrar cuántos se introdujeron.
+Primero con la clase scanner, luego con la clase JOptionPane */
+
+/*Ejercicio 5: Hacer un juego para adivinar un número entre 0-100 y luego ir pidiendo nros indicando "Es mayor", "Es menor"
+según sea mayor o menor con N, el juego termina cuándo el usuario acierta y mostramos la cantidad de intentos hechos. */
+
+import java.util.Scanner;
+
+int nro;
+
+int nros[];
+
+public class ejercicios_clase1y2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int nro;
+        System.out.println(nro);
+            int i=1
+            while (i>0){
+                add.nros[i];
+            }    
+    }
+
+}
