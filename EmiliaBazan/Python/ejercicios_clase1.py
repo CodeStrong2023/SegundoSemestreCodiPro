e8f2d6fed771950612e11456d27363a3f29b3919
Emilia Bazan <e.bazanazargado@gmail.com>
2023-09-18 22:20:07 -0300

    Actualizo archivos

---
+++ EmiliaBazan/Python/ejercicios_clase1.py
@@ -0,0 +1,7 @@
+#Ejercicio1: Iterar un rango de 0 a 10 e imprimir nros divisibles por 3.
+#Ejercicio2: Escribir un rango de nros de 2 a 6 e imprimirlos.
+#Ejercicio3: Crear un rango de 3 a 10 pero con incremento de 2 en 2. 
+
+""" Dada la siguiente tupla: 
+tupla = (13,1,8,3,2,5,8) , definir la tupla, crear ua lista e imprimir por consola los nros menores a 5."""
+
