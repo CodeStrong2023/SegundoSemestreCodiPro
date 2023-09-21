52676e9199623dbb9fb767855f0b517dee5a084f
Juan <juanberesiarte@gmail.com>
2023-09-21 18:39:11 -0300

    Aclarando los imports necesarios

--- beresiartejuan/programacion/java/Vanilla/src/App.java
+++ beresiartejuan/programacion/java/Vanilla/src/App.java
@@ -1,3 +1,8 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public final String EXERCISES_PACKAGE = "com.ejercicios";
