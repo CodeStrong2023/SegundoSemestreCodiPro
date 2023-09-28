4ca69bd61a35a15d9a673a4cc02414121ac31386
AVBalmaceda <aadriveross@gmail.com>
2023-09-28 17:56:38 +0200

    Ejercicio Python tema Pilas Semestre 2

--- Adry-Balmaceda/Python2-Adry-Balmaceda/Main_Leccion_Colecciones.py
+++ Adry-Balmaceda/Python2-Adry-Balmaceda/Main_Leccion_Colecciones.py
@@ -422,3 +422,16 @@ for llave, valor in seleccionArgentina.items():
 
 print('Actualmente hay cargados en el diccionario la cantidad de jugadores:', end='')
 print(len(seleccionArgentina))
+
+# Pilas: usando listas
+pila = [1, 2, 3]
+
+# Agregar elementos a la pila por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+
+# Sacamos elementos desde el final
+elementoBorrado = pila.pop()  # quita el ultimo elemento y lo guarda en la variable
+print(f'Sacamos el elemento: {elementoBorrado}')
+print(f'La pila ahora quedo asi: {pila}')
