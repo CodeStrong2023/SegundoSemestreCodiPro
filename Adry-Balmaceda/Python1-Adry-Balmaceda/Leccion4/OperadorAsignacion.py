885aacac30bc9f93fc9c3eeb3945e9d3ee322f5d
AVBalmaceda <aadriveross@gmail.com>
2023-09-28 17:55:17 +0200

    Repaso de ejercicios Python semestre 1

---
+++ Adry-Balmaceda/Python1-Adry-Balmaceda/Leccion4/OperadorAsignacion.py
@@ -0,0 +1,36 @@
+miVariable3 = 10
+print(miVariable3)
+
+# Operadores de reasignacion
+miVariable3 = miVariable3 +1
+print(miVariable3)
+
+# Sintasis reducida del incremento con reasignacion anterior
+miVariable3 += 1
+print(miVariable3)
+
+# miVariable3 = miVariable3 - 2
+miVariable3 -= 2
+print(miVariable3)
+
+# miVariable3 = miVariable3 * 3
+miVariable3 *= 3
+print(miVariable3)
+
+# miVariable3 = miVariable3 / 2
+miVariable3 /= 2
+print(miVariable3)
+
+# miVariable3 = miVariable3 % 2
+miVariable3 %= 2
+print(miVariable3)
+
+# Operadores que sirven para cadenas de texto
+# + concatenacion
+hola = "ho" + "la"
+print(hola)
+
+# * repeticion
+saludo = "hello world " * 4
+print(saludo)
+
