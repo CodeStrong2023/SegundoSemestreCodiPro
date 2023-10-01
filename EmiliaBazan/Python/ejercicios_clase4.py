2af8cc8a111b8e2cf70f74110af087a65f81629d
Emilia Bazan <e.bazanazargado@gmail.com>
2023-10-01 20:01:17 -0300

    Completo hasta clase 3

--- EmiliaBazan/Python/ejercicios_clase4.py
+++ EmiliaBazan/Python/ejercicios_clase4.py
@@ -8,10 +8,6 @@
 
 # print("")
 
-# array_unicos2 = []
-# [array_unicos2.append(x) for x in array if x not in array_unicos2]
-# print(array_unicos2)
-
 
 # Ejercicio2: Operaciones de conjuntos con listas. Escriba un programa que tenga dos listas y que
 # a continuación cree las siguientes listas (en las que no deben haber repeticiòn)
@@ -21,29 +17,30 @@
 # 4- Lista de palabras que aparecen en ambas listas
 
 lista1 = [3, 2, 6, "a", "b", "c", 33, 3.14, "as", "Vaso", "Moneda"]
-lista2 = [5, 2, 4, "t", "b", "d", 33, 3.16, "be", "Taza", "Pato"]
-lista3 = lista1 + lista2
-print("Ejercicio 2:")
+# lista2 = [5, 2, 4, "t", "b", "d", 33, 3.16, "be", "Taza", "Pato"]
+# lista3 = lista1 + lista2
+# print("Ejercicio 2:")
 
-print("1: Lista de palabras")
+# print("1: Lista de palabras")
 
-lista_str = []
-for elemento in lista3:
-    if isinstance(elemento, str):
-        lista_str.append(elemento)
-print(elemento)
+# lista_str = []
+# for elemento in lista3:
+#     if isinstance(elemento, str):
+#         lista_str.append(elemento)
+# print(elemento)
 
-print("2: Lista de palabras de lista 1")
 
-for elemento in lista1:
-    if isinstance(elemento, str):
-        print(elemento)
+# print("2: Lista de palabras de lista 1")
 
-print("3: Lista de palabras de lista 2")
+# for elemento in lista1:
+#     if isinstance(elemento, str):
+#         print(elemento)
 
-for elemento in lista2:
-    if isinstance(elemento, str):
-        print(elemento)
+# print("3: Lista de palabras de lista 2")
+
+# for elemento in lista2:
+#     if isinstance(elemento, str):
+#         print(elemento)
 
 
 # Ejercicio 3: Agregar personajes a una lista
@@ -60,3 +57,13 @@ for elemento in lista2:
 # Nombre: Legolas
 # Clase: Arquero
 # Raza: Elfo Sindar
+
+print('Ejercicio 3: Lista con personajes del Sr de los anillos')
+
+listaPersonajes = {
+    'Personaje1': {'Nombre': 'Aragon', 'Clase': 'Guerrero', 'Raza': 'Dunadan del norte'},
+    'Personaje2': {'Nombre': 'Gandalf', 'Clase': 'Mago', 'Raza': 'Istar'},
+    'Personaje3': {'Nombre': 'Legolas', 'Clase': 'Arquero', 'Raza': 'Elfo Sindar'},
+}
+
+print(listaPersonajes['Personaje1'])
--- EmiliaBazan/Python/ejerecicios_clase3.py
+++ EmiliaBazan/Python/ejerecicios_clase3.py
@@ -1,4 +1,35 @@
-# La tarea consiste en ingresar elementos al diccionario llamado seleccion Argentina, lo elementos a ingresar deben ser como mínimo 
-# 4, estos elementos son los jugadores con su número de camiseta, 
+# La tarea consiste en ingresar elementos al diccionario llamado seleccion Argentina, lo elementos a ingresar deben ser como mínimo
+# 4, estos elementos son los jugadores con su número de camiseta,
 # nombre, apellido, edad, altura, precio y posición de juego, por supuesto ver el video anterior.
 
+seleccionArg = {
+    10: {'Nombre': 'Lionel Messi', 'Edad': 35, 'Altura': 1.75, 'Precio': '50M', 'Posición': 'Delantero'},
+    11: {'Nombre': 'Angel Di María', 'Edad': 35, 'Altura': 1.80, 'Precio': '50M', 'Posición': 'Delantero'},
+    22: {'Nombre': 'Lautaro Martinez', 'Edad': 26, 'Altura': 1.77, 'Precio': '50M', 'Posición': 'Delantero'},
+    9: {'Nombre': 'Julián Álvarez', 'Edad': 23, 'Altura': 1.70, 'Precio': '50M', 'Posición': 'Delantero'},
+    21: {'Nombre': 'Paulo Dybala', 'Edad': 29, 'Altura': 1.77, 'Precio': '50M', 'Posición': 'Delantero'},
+    15: {'Nombre': 'Angel Correa', 'Edad': 28, 'Altura': 1.71, 'Precio': '50M', 'Posición': 'Delantero'},
+    7: {'Nombre': 'Rodrigo de Paul', 'Edad': 29, 'Altura': 1.77, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    5: {'Nombre': 'Leandro Paredes', 'Edad': 29, 'Altura': 1.82, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    20: {'Nombre': 'Alexis Mc Allister', 'Edad': 24, 'Altura': 1.74, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    18: {'Nombre': 'Guido Rodriguez', 'Edad': 29, 'Altura': 1.86, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    17: {'Nombre': 'Alejandro Gomez', 'Edad': 35, 'Altura': 1.67, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    24: {'Nombre': 'Enzo Fernández', 'Edad': 22, 'Altura': 1.78, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    14: {'Nombre': 'Excequiel Palacios', 'Edad': 24, 'Altura': 1.77, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    16: {'Nombre': 'Thiago Almada', 'Edad': 22, 'Altura': 1.71, 'Precio': '50M', 'Posición': 'Mediocampista'},
+    26: {'Nombre': 'Nahuel Molina', 'Edad': 25, 'Altura': 1.75, 'Precio': '50M', 'Posición': 'Defensor'},
+    4: {'Nombre': 'Gonzalo Montiel', 'Edad': 26, 'Altura': 1.75, 'Precio': '50M', 'Posición': 'Defensor'},
+    13: {'Nombre': 'Cristian Romero', 'Edad': 25, 'Altura': 1.85, 'Precio': '50M', 'Posición': 'Defensor'},
+    6: {'Nombre': 'Germán Pezzella', 'Edad': 32, 'Altura': 1.89, 'Precio': '50M', 'Posición': 'Defensor'},
+    19: {'Nombre': 'Nicolas Otamendi', 'Edad': 35, 'Altura': 1.83, 'Precio': '50M', 'Posición': 'Defensor'},
+    25: {'Nombre': 'Lisandro Martinez', 'Edad': 25, 'Altura': 1.75, 'Precio': '50M', 'Posición': 'Defensor'},
+    8: {'Nombre': 'Marcos Acuña', 'Edad': 31, 'Altura': 1.72, 'Precio': '50M', 'Posición': 'Defensor'},
+    3: {'Nombre': 'Nicolas Tagliafico', 'Edad': 31, 'Altura': 1.72, 'Precio': '50M', 'Posición': 'Defensor'},
+    2: {'Nombre': 'Juan Foith', 'Edad': 25, 'Altura': 1.87, 'Precio': '50M', 'Posición': 'Defensor'},
+    23: {'Nombre': 'Emiliano Martinez', 'Edad': 32, 'Altura': 1.95, 'Precio': '50M', 'Posición': 'Arquero'},
+    12: {'Nombre': 'Geronimo Rulli', 'Edad': 31, 'Altura': 1.88, 'Precio': '50M', 'Posición': 'Arquero'},
+    1: {'Nombre': 'Franco Armani', 'Edad': 36, 'Altura': 1.89, 'Precio': '50M', 'Posición': 'Arquero'},
+
+}
+
+print(seleccionArg[10])
