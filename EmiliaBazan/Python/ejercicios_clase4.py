9d97e28dec5f16da8b0ad0b55f3a6016aecca0a5
Emilia Bazan <e.bazanazargado@gmail.com>
2023-10-01 20:53:18 -0300

    Todos los ejercicios comentados en un mismo archivo, descomentar de a uno para ejecutar

--- EmiliaBazan/Python/ejercicios_clase4.py
+++ EmiliaBazan/Python/ejercicios_clase4.py
@@ -1,10 +1,14 @@
+import math
+
 # Ejercicio1: Eliminar duplicados de una lista - Escriba un programa dónde se tenga una lista y que
 # a continuación elimine los elementos repetidos. Por ùltimo, mostrar la lista.
 
 # print("Ejercicio 1:")
+
 # array = [1, 2, 2, 3, 4, 4, 5, "a", "a", "casa", "pez", 1, 3]
-# array_unicos = list(set(array))
-# print(array_unicos)
+# print('Lista con duplicados: ', array)
+# array_unicos = set(array)
+# print('Lista sin duplicados:  ', array_unicos)
 
 # print("")
 
@@ -16,32 +20,33 @@
 # 3- Lista de palabras que aparecen en la segunda lista pero no en la primera.
 # 4- Lista de palabras que aparecen en ambas listas
 
-lista1 = [3, 2, 6, "a", "b", "c", 33, 3.14, "as", "Vaso", "Moneda"]
+# lista1 = [3, 2, 6, "a", "b", "c", 33, 3.14, "as", "Vaso", "Moneda"]
 # lista2 = [5, 2, 4, "t", "b", "d", 33, 3.16, "be", "Taza", "Pato"]
-# lista3 = lista1 + lista2
-# print("Ejercicio 2:")
 
-# print("1: Lista de palabras")
+# conjunto1 = set(lista1)
+# conjunto2 = set(lista2)
 
-# lista_str = []
-# for elemento in lista3:
-#     if isinstance(elemento, str):
-#         lista_str.append(elemento)
-# print(elemento)
+# lista = list[conjunto1 | conjunto2]
+# print(lista)
 
+# # print("1: Lista de palabras")
 
-# print("2: Lista de palabras de lista 1")
+# lista = list[conjunto1 | conjunto2]
+# print(lista)
 
-# for elemento in lista1:
-#     if isinstance(elemento, str):
-#         print(elemento)
+# # print("2: Lista de palabras de lista 1")
 
-# print("3: Lista de palabras de lista 2")
+# unicos1 = [conjunto1 - conjunto2]
+# print(f'Lista de palabras de lista 1: {unicos1}')
 
-# for elemento in lista2:
-#     if isinstance(elemento, str):
-#         print(elemento)
+# # print("3: Lista de palabras de lista 2")
 
+# unicos2 = [conjunto2 - conjunto1]
+# print(f'Lista de palabras de lista 2: {unicos2}')
+
+# # print("4: Lista de palabras que aparecen en ambas listas")
+# compartidos = [conjunto2 & conjunto1]
+# print(f'Lista de palabras que aparecen en ambas listas: {compartidos}')
 
 # Ejercicio 3: Agregar personajes a una lista
 # Escriba una lista con los siguientes nombres de los personajes del señor de los anillos
@@ -58,12 +63,23 @@ lista1 = [3, 2, 6, "a", "b", "c", 33, 3.14, "as", "Vaso", "Moneda"]
 # Clase: Arquero
 # Raza: Elfo Sindar
 
-print('Ejercicio 3: Lista con personajes del Sr de los anillos')
+# print('Ejercicio 3: Lista con personajes del Sr de los anillos')
 
-listaPersonajes = {
-    'Personaje1': {'Nombre': 'Aragon', 'Clase': 'Guerrero', 'Raza': 'Dunadan del norte'},
-    'Personaje2': {'Nombre': 'Gandalf', 'Clase': 'Mago', 'Raza': 'Istar'},
-    'Personaje3': {'Nombre': 'Legolas', 'Clase': 'Arquero', 'Raza': 'Elfo Sindar'},
-}
+# listaPersonajes = {
+#     'Personaje1': {'Nombre': 'Aragon', 'Clase': 'Guerrero', 'Raza': 'Dunadan del norte'},
+#     'Personaje2': {'Nombre': 'Gandalf', 'Clase': 'Mago', 'Raza': 'Istar'},
+#     'Personaje3': {'Nombre': 'Legolas', 'Clase': 'Arquero', 'Raza': 'Elfo Sindar'},
+# }
 
-print(listaPersonajes['Personaje1'])
+# for valor in listaPersonajes.values():
+#     print(valor)
+
+# 4.4 Ejercicio 1 con Matemáticas y la clase math
+# Sacar la raíz cuadrada de un número positivo
+
+numero = int(input('Ingrese un número positivo: '))
+while numero < 0:
+    print("Error, ingrese nro positivo")
+    numero = int(input('Ingrese un número positivo: '))
+
+print(f'\nLa raíz cuadrada de {numero} es: {math.sqrt(numero):.2f}')
