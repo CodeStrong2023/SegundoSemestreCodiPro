1317d9697a16a1d77a0edc825bf63dd547b82b89
Emilia Bazan <e.bazanazargado@gmail.com>
2023-10-02 16:23:16 -0300

    Empezando clase 6

--- EmiliaBazan/Python/ejercicios_clase6.py
+++ EmiliaBazan/Python/ejercicios_clase6.py
@@ -13,31 +13,32 @@
 # 4 - Salir
 
 
-agenda = {}
-while True:
-    opcion = int(input(
-        "\nElija la opción que desea realizar: \n1. Crear contacto \n2. Borrar contacto \n3. Ver contacots \n4. Salir \n -"))
-    if opcion == 1:
-        nombre = input("Ingrese nombre: ")
-        telefono = input("Digite número de telefono: ")
-        if nombre not in agenda:
-            agenda[nombre] = telefono
-            print('Contacto agregado sactifactoriamente!')
-        else:
-            print('Contacto existente!')
-    elif opcion == 2:
-        nombre = input('Ingrese el nombre del contacto que desea eliminar: ')
-        if nombre in agenda:
-            del (agenda[nombre])
-            print("Se eliminó el contacto de la lista")
-        else:
-            print("Contacto inexistente")
-    elif opcion == 3:
-        print('Agenda:')
-        for clave, valor in agenda.items():
-            print(f'Nombre: {clave}, Telefono: {valor}')
-    elif opcion == 4:
-        print('Gracias por usar la agenda')
-        break
-    else:
-        print('Dato inválido, vuelva a ingresar')
+# agenda = {}
+# while True:
+#     opcion = int(input(
+#         "\nElija la opción que desea realizar: \n1. Crear contacto \n2. Borrar contacto \n3. Ver contacots \n4. Salir \n -"))
+#     if opcion == 1:
+#         nombre = input("Ingrese nombre: ")
+#         telefono = input("Digite número de telefono: ")
+#         if nombre not in agenda:
+#             agenda[nombre] = telefono
+#             print('Contacto agregado sactifactoriamente!')
+#         else:
+#             print('Contacto existente!')
+#     elif opcion == 2:
+#         nombre = input('Ingrese el nombre del contacto que desea eliminar: ')
+#         if nombre in agenda:
+#             del (agenda[nombre])
+#             print("Se eliminó el contacto de la lista")
+#         else:
+#             print("Contacto inexistente")
+#     elif opcion == 3:
+#         print('Agenda:')
+#         for clave, valor in agenda.items():
+#             print(f'Nombre: {clave}, Telefono: {valor}')
+#     elif opcion == 4:
+#         print('Gracias por usar la agenda')
+#         break
+#     else:
+#         print('Dato inválido, vuelva a ingresar')
+
