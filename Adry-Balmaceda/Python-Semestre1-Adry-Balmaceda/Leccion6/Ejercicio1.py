e5f93f6bc18c7e8c497f29c8b1e72778a455d904
AVBalmaceda <aadriveross@gmail.com>
2023-10-07 02:01:52 +0200

    Reorganización de archivos

--- Adry-Balmaceda/Python-Semestre1-Adry-Balmaceda/Leccion6/Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-'''Ejercicio 1:
-Deberemos plasmar la expresion en una expresion
-algoritmica, o sea hacerlo en codigo
-'''
-a = float(input('Digite el valor de a: '))
-b = float(input('Digite el valor de b: '))
-c = float(input('Digite el valor de c: '))
-resultado = (a ** 3 * (b ** 2 - 2 * a * c)) / (2 * b)
-print(f'El resultado es : {resultado}')
