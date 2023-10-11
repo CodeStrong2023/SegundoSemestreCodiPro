6aafd1bdfed27344e498ad0dab5d753ca2adb127
Fabri Martinez <fabrimartinez66@gmail.com>
2023-10-10 22:45:26 -0300

    Agrego octava clase de Java y septima clase de Python y Javascript

---
+++ TrabajosProgramacionMartinezFabricio/javascript/Clase07/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares e impares
+let parImpar = 7;
+if(parImpar%2==0){
+    console.log("El número es PAR");
+}
+else{
+    console.log("El número es IMPAR");
+}
+
+//Ejercicio es mayor de edad
+let edad = 17;
+if(edad>=18){
+    console.log("Es mayor de edad")
+}
+else{
+    console.log("Es menor de edad")
+}
