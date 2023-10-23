628a26a2207b29e32a7b736927c2666672edd7a7
Emilia Bazan <e.bazanazargado@gmail.com>
2023-10-23 20:51:21 -0300

    Clase 7 Java completa

--- EmiliaBazan/Java/Leccion 5/Encapsulamiento/src/dominio/Persona.java
+++ EmiliaBazan/Java/Leccion 5/Encapsulamiento/src/dominio/Persona.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dominio;
 
-/**
- *
- * @author ebaza
- */
+
 public class Persona {
+    //Atributos
+    private String nombre;
+    private double sueldo;
+    boolean eliminado;
+    
+    //constructor
+    public Persona(String nombre, double sueldo, boolean eliminado){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+    }
+    
+    //Método
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
     
 }
