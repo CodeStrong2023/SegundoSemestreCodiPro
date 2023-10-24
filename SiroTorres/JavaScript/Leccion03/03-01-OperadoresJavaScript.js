c61d1a7d204813c7e2ce2a66278e1559297bf99c
Siro01 <sirooo444@gmail.com>
2023-10-24 17:03:56 -0300

    Ejercicios JavaScript segundo semestre
    ejercicios hasta la fecha 24-10

---
+++ SiroTorres/JavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,18 @@
+//Ejercicios para encontrar numeros pares.
+let parInpar = 2;
+if(parInpar % 2 == 0){
+    console.log("Es un numero PAR");
+}
+else{
+    console.log("Es un numero IMPAR")
+}
+
+//Ejercicio: es mayor de edad.
+let edad = 20, adulto = 18;
+if( edad >= adulto){
+    console.log('Es una persona Adulta');
+
+}
+else{
+    console.log('Es menor de edad')
+}
