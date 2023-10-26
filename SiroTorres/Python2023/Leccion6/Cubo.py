1cd0589868b4ac70fb4d23ef34e1d572021e83d1
Siro01 <sirooo444@gmail.com>
2023-10-26 16:29:00 -0300

    Clase 9 de Pyhton POO parte 2.

---
+++ SiroTorres/Python2023/Leccion6/Cubo.py
@@ -0,0 +1,19 @@
+class Cubo:
+    """
+    Crear la clase Cubo con los atributos, ancho, alto y profundidad, con un
+    metodo calcular_volumen que tendra la formula:
+    volumen = ancho * altura * profunidad
+    que el usuario ingrese los valores
+    """
+    def __init__(self, ancho, altura, profundidad):
+        self.ancho = ancho
+        self.altura = altura
+        self.profundidad = profundidad
+
+    def volumen(self):
+        return self.ancho * self.altura *self.profundidad
+ancho = int(input('Digite el ancho del cubo: '))
+altura = int(input('Digite la altura del cubo: '))
+profundidad = int(input('Digite la profundidad del cubo: '))
+cubo1 = Cubo(ancho, altura, profundidad)
+print(f'El volumen del cubo es: {cubo1.volumen()}')
