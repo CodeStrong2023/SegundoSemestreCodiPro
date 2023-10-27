45af6af4db50865645f865e6562a97d869de4051
AdrianaVB <aadriveross@gmail.com>
2023-10-27 07:36:19 -0300

    Repaso de las lecciones de Python

---
+++ AdryBalmaceda/Python2-Adry-Balmaceda/Leccion5/main.py
@@ -0,0 +1,10 @@
+ # Funciones
+# Necesitan ser llamadas para que se use el codigo de la funcion
+
+# mi_funcion() # no se puede llamar antes de definir una funcion
+
+# 1 definir una funcion, usaremos estilo de escritura snake_case
+def mi_funcion():  # para identificar la funcion se usa parentesis
+    print('Saludos a todos los profesores de la Tecnicactura')
+
+mi_funcion()  # estamos llamando a la funcion y se puede llamar n cantidad de veces
