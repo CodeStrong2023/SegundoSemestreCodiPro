ee54dbb949f7c609b3c60554d1d23464f8f827c3
AdrianaVB <aadriveross@gmail.com>
2023-10-31 23:58:28 -0300

    Ejercicios con operadores en JavaScript

--- AdryBalmaceda/JavaScript-Adry-Balmaceda/Leccion3/03-01-OperadoresJavaScript-UJS.js
+++ AdryBalmaceda/JavaScript-Adry-Balmaceda/Leccion3/03-01-OperadoresJavaScript-UJS.js
@@ -16,7 +16,7 @@ else{
     console.log('Es menor de edad');
 }
 
-// Ejercicio: dentro de un rango
+// Ejercicio: dentro de un rango, operador &&
 let dentroRango = 10; // aqui vamos a ir cambiando el valor
 let valMin = 0, valMax = 10;
 if(dentroRango >= valMin && dentroRango <= valMax){
@@ -25,3 +25,50 @@ if(dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log('Esta fuera del rango establecido')
 }
+
+/*
+Ejercicio: Si el padre puede asistir al juego de su hijo
+con operador or ||
+*/
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log('El padre puede asistir al juego de su hijo');
+}
+else{
+    console.log('El padre NO puede asistir al juego de su hijo');
+}
+
+// Operador ternario parecido a if else, es para codigos cortos, para codigos largos se utiliza if else
+let resultado2 = 3 > 2 ? "Verdadero": "Falso";
+console.log(resultado2);
+let numero = 6;
+resultado2 = numero % 2 == 0 ? "Es un numero PAR": "Es un numero IMPAR";
+console.log(resultado2);
+
+// Convertir String a Number
+let miNumero = "19";   // es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);  // esta es una funcion que va a hacer la conversion
+console.log(typeof edad2);
+
+// Funcion isNaN, para verificar el valor de una variable de tipo numerica
+// compara numero con numero y nos da un resultado booleano
+if(isNaN(edad2)){  // no es un numero = is Not a Number(devuelve un resultado booleano)
+    console.log('Esta variable no contiene solo numeros');
+}
+else{
+    if(edad2 >= 18){
+    console.log('Puede votar');
+    }
+    else{
+        console.log('Muy joven para votar');
+    }
+}
+
+
+// Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar": "Muy joven para votar";
+console.log(resultado3);
+
+
+
--- AdryBalmaceda/JavaScript-Adry-Balmaceda/Leccion4/04-01-Ejercicios.js
+++ AdryBalmaceda/JavaScript-Adry-Balmaceda/Leccion4/04-01-Ejercicios.js
@@ -1,3 +1,54 @@
+// Ejercicio 1: Calcular estacion del año, sintaxis igual a Java
+let mes = 10;
+let estacion; // undefined, ya está definida
+if(mes == 1 || mes == 2 || mes == 12){
+    estacion = 'Verano';
+}
+else if(mes == 3 || mes == 4 || mes == 5){
+    estacion = 'Otoño';
+}
+else if(mes == 6 || mes == 7 || mes == 8){
+    estacion ='Invierno';
+}
+else if(mes == 9 || mes == 10 || mes == 11){
+    estacion = 'Primavera';
+}
+else{
+    estacion = 'Valor incorrecto';
+}
+console.log('El valor corresponde a la estacion de: '+estacion);
+
+// Ejercicio 2: Hora del día
+/**
+ * Rutinas
+ * Hora de levantarse: 7 a 8
+ * Hora de visita de obra: 9 a 15
+ * Hora de oficina: 16 a 19
+ * Hora de clases: 20 a 23
+ * Hora de dormir: 0 a 6
+ */
+let hora = 8;
+let rutina;
+if(hora >= 7 && hora <= 8){
+    rutina = 'Hora de levantarse';
+}
+else if(hora >= 9 && hora <= 15){
+    rutina = 'Hora de visita de obra';
+}
+else if(hora >= 16 && hora <= 19){
+    rutina = 'Hora de oficina';
+}
+else if(hora >= 20 && hora <= 23){
+    rutina = 'Hora de clases';
+}
+else if(hora >= 0 && hora <= 6){
+    rutina = 'Hora de dormir';
+}
+else{
+    rutina = 'Opcion incorrecta';
+}
+console.log(rutina);
+
 /**
  * Ampliando el uso de var let y const
  * con var puedes reasignar en cualquier momento
