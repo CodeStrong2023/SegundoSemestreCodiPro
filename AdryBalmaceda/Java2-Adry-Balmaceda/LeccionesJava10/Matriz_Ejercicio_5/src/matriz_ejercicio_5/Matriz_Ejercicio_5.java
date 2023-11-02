90c3f59561efc2f599b01d89a2bd4bfdef5daadd
AdrianaVB <aadriveross@gmail.com>
2023-11-02 15:34:11 -0300

    Repaso Ejercicios con matrices en Java

--- AdryBalmaceda/Java2-Adry-Balmaceda/LeccionesJava10/Matriz_Ejercicio_5/src/matriz_ejercicio_5/Matriz_Ejercicio_5.java
+++ AdryBalmaceda/Java2-Adry-Balmaceda/LeccionesJava10/Matriz_Ejercicio_5/src/matriz_ejercicio_5/Matriz_Ejercicio_5.java
@@ -14,16 +14,16 @@ public class Matriz_Ejercicio_5 {
 
     public static void main(String[] args) {
         int nF = Integer.parseInt(JOptionPane.showInputDialog("Ingresar el numero de filas para llenar la matriz"));
-        int nC = Integer.parseInt(JOptionPane.showInputDialog("Ingresar el numero de columnas para llenar la matriz"));
+        int mC = Integer.parseInt(JOptionPane.showInputDialog("Ingresar el numero de columnas para llenar la matriz"));
         int posF;
         int posC;
         int sumaF;
         int sumaC;
 
-        int matriz[][] = new int[nF][nC];
+        int matriz[][] = new int[nF][mC];
         // Para llenar la matriz
         for (int i = 0; i < nF; i++) {
-            for (int j = 0; j < nC; j++) {
+            for (int j = 0; j < mC; j++) {
                 matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero de posiciones para filas y columnas: [" + (i + 1) + "," + (j + 1) + "]"));
 
             }
@@ -31,7 +31,7 @@ public class Matriz_Ejercicio_5 {
         // Imprimir matriz
         String resultado = " ";
         for (int i = 0; i < nF; i++) {
-            for (int j = 0; j < nC; j++) {
+            for (int j = 0; j < mC; j++) {
                 resultado += matriz[i][j];
                 resultado += " ";
             }
@@ -41,17 +41,38 @@ public class Matriz_Ejercicio_5 {
 
         // Sumar fila
         int filas[] = new int[nF];
-        int columnas[] = new int[nC];
+        int columnas[] = new int[mC];
         posF = 0;
         for (int i = 0; i < nF; i++) {
             sumaF = 0;
-            for (int j = 0; j < nC; j++) {
+            for (int j = 0; j < mC; j++) {
                 sumaF += matriz[i][j];
             }
             filas[posF] = sumaF;
             posF++;
         }
-        JOptionPane.showInternalMessageDialog(null, posF);
+        
         // Sumar columnas
+        posC = 0;
+        for (int i = 0; i < nF; i++) {
+            sumaC=0;
+            for (int j = 0; j < mC; j++) {
+                sumaC += matriz[i][j];
+            }
+            columnas[posC]= sumaC;
+            posC++;
+        }
+        System.out.println("\nSuma Filas");
+        for (int i = 0; i < nF; i++) {
+            System.out.println(filas[i]+"-");
+            
+        }
+        
+        
+        System.out.println("\nSuma Columnas");
+        for (int j = 0; j < mC; j++) {
+            System.out.println(columnas[j]+"-");
+            
+        }
     }
 }
