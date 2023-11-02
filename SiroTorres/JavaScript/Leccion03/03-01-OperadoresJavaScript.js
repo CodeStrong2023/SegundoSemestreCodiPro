20933ffb6c7662f940592a846b83dc7bc205e89c
Siro01 <sirooo444@gmail.com>
2023-11-02 15:41:06 -0300

    Clase 8 de JavaScript

--- SiroTorres/JavaScript/Leccion03/03-01-OperadoresJavaScript.js
+++ SiroTorres/JavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -16,3 +16,13 @@ if( edad >= adulto){
 else{
     console.log('Es menor de edad')
 }
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 5; //Aqui vamos a ir cambiando el valoor
+let valMin = 0, valMax = 10;
+if( dentroRango >= valMin && dentroRango < valMax){
+    console.log('Esta dentro del rango establecido')
+}
+else{
+    console.log('Esta fuera del rango establecido')
+}
